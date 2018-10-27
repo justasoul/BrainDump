cb109399aa4e96c9d18e33dd6a6175a6377f237c
wiki <wiki@16f03da7e781>
2018-10-27 09:53:59 +0000

    autocommit @ Sat Oct 27 09:53:59 UTC 2018

---
+++ conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,separated
+
+
+admin:$1$4H7djxEY$ldnV2g8BKF3qUwdGjN9dk.:administrator:justasoul@gmail.com:admin,user
